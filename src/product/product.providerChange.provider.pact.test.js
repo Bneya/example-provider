a9dbedc8637727cdef0376b0e64cc958e86bd5fb
Bneya <biriveravidal@gmail.com>
2024-04-19 12:02:45 -0400

    probar a master sin ajustar - debería fallar

--- src/product/product.providerChange.provider.pact.test.js
+++ src/product/product.providerChange.provider.pact.test.js
@@ -28,32 +28,42 @@ describe('Pact Verification', () => {
     // For 'normal' provider builds, fetch the the latest version from the main branch of each consumer, as specified by
     // the consumer's mainBranch property and all the currently deployed and currently released and supported versions of each consumer.
     // https://docs.pact.io/pact_broker/advanced_topics/consumer_version_selectors
-    const fetchPactsDynamicallyOpts = {
-      provider: 'pactflow-example-provider',
-      // consumerVersionSelectors: [
-      //   // { mainBranch: true },
-      //   // { deployed: true },
-      //   // { matchingBranch: true }
-      //   process.env.CONSUMER_TARGET_BRANCH
-      //     ? { branch: process.env.CONSUMER_TARGET_BRANCH }
-      //     : { environment: process.env.ENVIRONMENT, deployed: true }
-      // ],
-      // pactUrls: [process.env.PACT_URL],
-      pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
-      // https://docs.pact.io/pact_broker/advanced_topics/pending_pacts
-      enablePending: true,
-      // https://docs.pact.io/pact_broker/advanced_topics/wip_pacts
-      includeWipPactsSince: '2020-01-01'
-    };
 
-    if (process.env.PACT_URL) {
-      fetchPactsDynamicallyOpts['pactUrls'] = [process.env.PACT_URL]
-    } else {
-      fetchPactsDynamicallyOpts['consumerVersionSelectors'] = [
-        process.env.CONSUMER_TARGET_BRANCH
-          ? { branch: process.env.CONSUMER_TARGET_BRANCH }
-          : { environment: process.env.ENVIRONMENT, deployed: true }
-      ]
+    // Intento antiguo
+    // const fetchPactsDynamicallyOpts = {
+    //   provider: 'pactflow-example-provider',
+    //   // consumerVersionSelectors: [
+    //   //   // { mainBranch: true },
+    //   //   // { deployed: true },
+    //   //   // { matchingBranch: true }
+    //   //   process.env.CONSUMER_TARGET_BRANCH
+    //   //     ? { branch: process.env.CONSUMER_TARGET_BRANCH }
+    //   //     : { environment: process.env.ENVIRONMENT, deployed: true }
+    //   // ],
+    //   // pactUrls: [process.env.PACT_URL],
+    //   pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
+    //   // https://docs.pact.io/pact_broker/advanced_topics/pending_pacts
+    //   enablePending: true,
+    //   // https://docs.pact.io/pact_broker/advanced_topics/wip_pacts
+    //   includeWipPactsSince: '2024-04-01'
+    // };
+
+    // if (process.env.PACT_URL) {
+    //   fetchPactsDynamicallyOpts['pactUrls'] = [process.env.PACT_URL]
+    // } else {
+    //   fetchPactsDynamicallyOpts['consumerVersionSelectors'] = [
+    //     process.env.CONSUMER_TARGET_BRANCH
+    //       ? { branch: process.env.CONSUMER_TARGET_BRANCH }
+    //       : { environment: process.env.ENVIRONMENT, deployed: true }
+    //   ]
+    // }
+
+    // Intento 2024-04-19
+    const fetchPactsDynamicallyOpts = {
+      // branch: process.env.CONSUMER_TARGET_BRANCH
+      branch: 'master',  // hardcodeado para probar
+      enablePending: true,
+      ncludeWipPactsSince: '2024-04-01'
     }
 
     const opts = {
